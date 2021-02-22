9ef5d38016aae2b565abbdc42bbc956f07f559a5
Aleksei3232 <mamont3232@gmail.com>
2021-02-22 17:07:20 +0300

    prime numbers divide

---
+++ prime-divider.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void divider(int N, int div);
+int main()
+{
+
+    int N = 256;
+    divider(N, 2);
+    return 0;
+}
+
+void divider(int N, int div)
+{
+
+    if (N <= div){
+        printf("%d \n", div);
+        return;
+    }
+
+    if (N % div == 0){
+        printf("%d \n", div);
+        return divider ( N / div, div);
+    }
+
+    if (N % div > 0){
+        return divider(N, ++div);
+    }
+}
+    
+
+
